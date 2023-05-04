3b08aa1326757cf153a3523e9e3ea831182c3f83
Josh Shepherd <joshuashepherd877@gmail.com>
2023-05-04 18:32:11 +0100

    add user schema

--- core/models/user.py
+++ core/models/user.py
@@ -1,4 +1,36 @@
 from ..extensions import db
+from datetime import datetime
 
 class User(db.Model):
-    pass
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(64), index=True, unique=True)
+    email = db.Column(db.String(120), index=True, unique=True)
+    password_hash = db.Column(db.Text)
+    orders = db.relationship()
+    street_address = db.Column(db.String(50))
+    city = db.Column(db.String(50))
+    state = db.Column(db.String(50))
+    postcode = db.Column(db.String(20))
+    country = db.Column(db.String(50))
+    phone = db.Column(db.String(20))
+    created_at = db.Column(db.DateTime, index=True, default=datetime.utcnow)
+
+    def to_dict(self):
+        return {
+            'id': self.id,
+            'username': self.username,
+            'email': self.email,
+            'orders': self.orders,
+            'street_address': self.street_address,
+            'city': self.city,
+            'state': self.state,
+            'postcode': self.postcode,
+            'country': self.country,
+            'phone': self.phone,
+            'created_at': self.created_at
+        }
+    
+    def __repr__(self):
+        return '<User {}>'.format(self.username)
+    
+    
