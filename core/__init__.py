fc94561487d4b0f42bada83ba2a3fecda2c65d9d
Josh Shepherd <joshuashepherd877@gmail.com>
2023-05-04 17:55:51 +0100

    add basic blueprints

--- core/__init__.py
+++ core/__init__.py
@@ -3,4 +3,13 @@ from flask import Flask
 def create_app():
     app = Flask(__name__)
 
+    # register blueprints
+    from routes import users as users_bp,\
+                    products as product_bp,\
+                    orders as order_bp
+    app.register_blueprint(users_bp)
+    app.register_blueprint(product_bp)
+    app.register_blueprint(order_bp)
+    
+
     return app
---
+++ core/routes/__init__.py
@@ -0,0 +1,3 @@
+from .orders import orders
+from .products import products
+from .users import users
